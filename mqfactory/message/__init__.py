21d556391225452f54a9606a48243201de68a1ce
Christophe VG <contact@christophe.vg>
2019-01-28 22:23:55 +0100

    allow for specifying tags upon Message creation

--- mqfactory/message/__init__.py
+++ mqfactory/message/__init__.py
@@ -1,8 +1,8 @@
 class Message(object):
-  def __init__(self, to, payload):
+  def __init__(self, to, payload, tags=None):
     self.to      = to
     self.payload = payload
-    self.tags    = {}
+    self.tags    = tags or {}
 
   def __iter__(self):
     yield ("to",      self.to)
