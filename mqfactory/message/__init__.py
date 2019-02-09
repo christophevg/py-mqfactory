43079c5281ab8b5f0492c4dbed684895675ac183
Christophe VG <contact@christophe.vg>
2019-02-09 13:36:45 +0100

    added self-copy method to Message

--- mqfactory/message/__init__.py
+++ mqfactory/message/__init__.py
@@ -1,8 +1,17 @@
+import copy
+
 class Message(object):
   def __init__(self, to, payload, tags=None):
     self.to      = to
     self.payload = payload
     self.tags    = tags or {}
+
+  def copy(self):
+    return Message(
+      self.to,
+      copy.deepcopy(self.payload),
+      copy.deepcopy(self.tags)
+    )
 
   def __iter__(self):
     yield ("to",      self.to)
