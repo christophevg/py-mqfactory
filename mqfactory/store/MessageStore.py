8235a4d617d687e903d9125fb86e2b39e688ff58
Christophe VG <contact@christophe.vg>
2019-02-11 23:58:26 +0100

    added more debug logging

--- mqfactory/store/MessageStore.py
+++ mqfactory/store/MessageStore.py
@@ -1,3 +1,5 @@
+import logging
+
 from mqfactory.message import Message
 
 class MessageStore(object):
@@ -10,6 +12,7 @@ class MessageStore(object):
 
   def after_add(self, outbox, message):
     message.private["id"] = self.collection.add(dict(message))
+    logging.debug("message {0} store as {1}".format(message.id, message.private["id"]))
 
   def before_remove(self, outbox, message):
     self.load_messages(outbox)
@@ -21,7 +24,10 @@ class MessageStore(object):
     self.load_messages(outbox)
 
   def after_defer(self, outbox, message):
-    self.collection.update(message.private["id"], dict(message))
+    try:
+      self.collection.update(message.private["id"], dict(message))
+    except:
+      logging.error("after_defer update failed for {0}".format(str(message)))
 
   def load_messages(self, outbox):
     if not self.loaded:
